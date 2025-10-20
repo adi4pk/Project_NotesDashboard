0120e5aac9f8a2efccc57a17fb81cd270d2dc0ab
Adrian Stancioiu <adrian.stancioiu96@yahoo.com>
2025-10-20 21:39:43 +0300

    3rd commit

--- app.js
+++ app.js
@@ -1,61 +1,141 @@
+//variabile globale
+let currentPage = "allEvents";
+
 //selectors
-
-let cardContainer=document.querySelector(".card-container");
-let addNotesBtn=document.querySelector(".add-notes-btn");
+let cardContainer = document.querySelector(".card-container");
+let addNotesBtn = document.querySelector(".add-notes-btn");
 let modalContainer = document.querySelector(".modal-container");
-let modalBtnAdd=document.querySelector(".modal-btn-add");
-let modalBtnDiscard=document.querySelector(".modal-btn-discard");
+let modalBtnAdd = document.querySelector(".modal-btn-add");
+let modalBtnDiscard = document.querySelector(".modal-btn-discard");
+let cardType = modalContainer.querySelector(".select-card-type");
+let navContainer = document.querySelector(".nav-container");
+let allEvents = navContainer.querySelector(".all-notes");
+let favEvents = navContainer.querySelector(".favorite-notes");
 
+let businessSection = navContainer.querySelector(".business-par");
+let socialSection = navContainer.querySelector(".social-par");
+let importantSection = navContainer.querySelector(".imp-par");
+let evenimenteOriginal = evenimente;
+let error = cardContainer.querySelector(".error-no-events");
+let noData = document.querySelector(".no-data");
 
-addNotesBtn.addEventListener("click", () =>{
-    
-    modalContainer.classList.add("show");
-});
-
-modalBtnAdd.addEventListener("click", () =>{
-    modalContainer.classList.remove("show");
-    addCard();
-    
-});
-
-modalBtnDiscard.addEventListener("click", () =>{
-    modalContainer.classList.remove("show");
-});
-
+//cod de initializare
 attachCards(evenimente);
 
-cardContainer.addEventListener("click", (ev)=>{
-    let obj=ev.target;
-    let card = obj.closest(".card");    
+//events
+addNotesBtn.addEventListener("click", () => {
+  modalContainer.classList.add("show");
+});
 
+modalBtnAdd.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+  saveEvent();
 
-    if(obj.classList.contains("trash")){
-        
-      let data=(obj.parentNode.parentNode).className.split(" ")[1].split("-")[1];
-      evenimente=deleteEvenimentById(evenimente,data);
+  // if(cardType.childNodes[1].value === "business"){
+  //     objCard.assign({type: "business"});
+  // }
+
+  // if(cardType.childNodes[1].value === "business"){
+  //     objCard.type = "business";
+  // };
+
+  console.log(currentPage);
+  switch (currentPage) {
+    case "allEvents":
       attachCards(evenimente);
+      break;
+    case "businessSection":
+      bus = showBus(evenimente);
+      attachCards(bus);
+      break;
+    case "socialSection":
+      soc = showSoc(evenimente);
+      attachCards(soc);
+      break;
+    case "importantSection":
+      imp = showImp(evenimente);
+      attachCards(imp);
+      break;
+    case "favEvents":
+      attachCards(evenimente);
+      break;
+    default:
+      attachCards(evenimente);
+  }
+});
 
-    }else if(obj.classList.contains("star")){
-        favoriteCard(obj);
-    }else if(obj.classList.contains("edit")){
-        
-        let data=obj.parentNode.parentNode;
-        // console.log(data);
-        editCard(obj);
-        
-        
-        
-    }else if(obj.classList.contains("save")){
-        let btnSave = card.querySelector(".save");
-        let parent = obj.parentNode;
-        
-        let btnEdit = document.createElement("p");
-        saveCard(card);
-        btnSave.classList.toggle("hide");
-        btnSave.classList.toggle("inline");
+modalBtnDiscard.addEventListener("click", () => {
+  modalContainer.classList.remove("show");
+});
 
-        parent.appendChild(btnEdit);
-        btnEdit.classList.add("edit");
-        btnEdit.textContent=("EDIT");
-    };
+// allEvents.addEventListener("click", () => {
+//   handleFilterByType(evenimente);
+//   currentPage = "allEvents";
+// });
+
+// businessSection.addEventListener("click", () => {
+//   handleFilterByType(filterByType(evenimente, "business"));
+//   currentPage = "businessSection";
+// });
+
+// socialSection.addEventListener("click", () => {
+//   handleFilterByType(filterByType(evenimente, "social"));
+//   currentPage = "socialSection";
+// });
+
+// importantSection.addEventListener("click", () => {
+//   handleFilterByType(filterByType(evenimente, "important"));
+//   currentPage = "importantSection";
+// });
+
+//
+
+navContainer.addEventListener("click", (ev) =>{
+    let obj = ev.target;
+
+    if(obj.classList.contains("all-notes")){
+        handleFilterByType(evenimente);
+        currentPage = "allEvents";
+    }else if(obj.classList.contains("business-par")){
+        handleFilterByType(filterByType(evenimente, "business"));
+        currentPage = "businessSection";
+    }else if(obj.classList.contains("social-par")){
+        handleFilterByType(filterByType(evenimente, "social"));
+        currentPage = "socialSection";
+    }else if(obj.classList.contains("imp-par")){
+        handleFilterByType(filterByType(evenimente, "important"));
+        currentPage = "importantSection";
+    }else if(obj.classList.contains("favorite-notes")){
+      handleFilterByType(filterByFav(evenimente))
+      currentPage = "favEvents";
+    }
+})
+
+cardContainer.addEventListener("click", (ev) => {
+  let obj = ev.target;
+  let card = obj.closest(".card");
+
+  if (obj.classList.contains("trash")) {
+    let data = obj.parentNode.parentNode.className.split(" ")[1].split("-")[1];
+    evenimente = deleteEvenimentById(evenimente, data);
+    attachCards(evenimente);
+  } else if (obj.classList.contains("star")) {      //favorite
+    favoriteCard(obj.parentNode.parentNode);
+
+  } else if (obj.classList.contains("edit")) {
+    let data = obj.parentNode.parentNode;
+    editCard(data);
+  } else if (obj.classList.contains("save")) {
+    let btnSave = card.querySelector(".save");
+    let parent = obj.parentNode;
+
+    let btnEdit = document.createElement("p");
+    saveCard(card);
+    btnSave.classList.toggle("hide");
+    btnSave.classList.toggle("inline");
+
+    parent.appendChild(btnEdit);
+    btnEdit.classList.add("edit");
+    btnEdit.textContent = "EDIT";
+  }
 });
--- functions.js
+++ functions.js
@@ -1,119 +1,149 @@
 //functie ce creaza un card primeste ca parametru
 
+function createCard(evenim) {
+  let card = document.createElement("div");
 
-function createCard(evenim){
-
-    let card=document.createElement("div");
-
-    card.classList.add("card");
-    card.classList.add(`card-${evenim.id}`)
-    card.innerHTML=`
+  card.classList.add("card");
+  card.classList.add(`card-${evenim.id}`);
+  card.innerHTML = `
                         <h3>${evenim.titlu}</h3>
                         <p>${evenim.data}</p>
 
-                        <p>${evenim.descriere}Lorem ipsum dolor sit amet consectetur.</p>
-                        <div >
-                            <i class="favorite fa-solid fa-star"></i>
+                        <p class="card-desc">${evenim.descriere}Lorem ipsum dolor sit amet consectetur.</p>
+                        <div class="button-container">
+                            <i class="star fa-solid fa-star"></i>
                             <i class="trash fa-solid fa-trash"></i>
                             <p class="edit">EDIT</p>
                             <p class="save hide">SAVE</p>
                         </div>
                   `;
-    
-    return card;
+
+  if (evenim.isFavorite === true) {
+    card.classList.add("favorite");
+  }
+
+
+  return card;
 }
 
 //functie ce populeaza pagina cu carduri
 // array argument
 
-function attachCards(arr){
-
-    let cardContainer = document.querySelector(".card-container");
-    cardContainer.innerHTML="";
-    arr.map(e=>createCard(e)).forEach(element => {
-        
-        cardContainer.append(element);
+function attachCards(arr) {
+  let cardContainer = document.querySelector(".card-container");
+  cardContainer.innerHTML = "";
+  arr
+    .map((e) => createCard(e))
+    .forEach((element) => {
+      cardContainer.append(element);
     });
 }
 
-function addCard(){
+function saveEvent() {
+  let cardContainer = document.querySelector(".card-container");
+  modalContainer = document.querySelector(".modal-container");
 
-    modalContainer = document.querySelector(".modal-container");
-    let noteTitle = modalContainer.querySelector(".input-title-modal");
-    let noteDesc = modalContainer.querySelector(".text-area-modal");
+  let noteTitle = modalContainer.querySelector(".input-title-modal");
+  let noteDesc = modalContainer.querySelector(".text-area-modal");
 
+  let cardType = modalContainer.querySelector(".select-card-type"); //picks up EXACTLY the selected option from the menu.
 
-    let card=document.createElement("div");
+  let objCard = {
+    id: evenimente.length + 1,
+    titlu: noteTitle.value,
+    data: "2025-10-15",
+    descriere: noteDesc.value,
+    type: cardType.value,
+    isFavorite: false
+  };
 
-    card.classList.add("card");
-    card.classList.add(`card-${evenimente.length +1}`)
-    card.innerHTML=`
-                        <h3>${noteTitle.value}</h3>
-                        <p>test data</p>
+  noteTitle.value = "";
+  noteDesc.value = "";
+  cardType.value = "";
 
-                        <p>${noteDesc.value}Lorem ipsum dolor sit amet consectetur.</p>
-                        <div >
-                            <i class="favorite fa-solid fa-star"></i>
-                            <i class="trash fa-solid fa-trash"></i>
-                            <p class="edit">EDIT</p>
-                            <p class="save hide">SAVE</p>
-                        </div>
-                  `;
-    evenimente.push(card);
-    console.log(noteDesc.value);
+  evenimente.push(objCard);
 }
 
+function favoriteCard(card) {
+  const cardId = parseInt(card.className.split(" ")[1].split("-")[1]);
+  let eventId = findEvenimentById(evenimente, cardId);
+  if (eventId !== undefined) {
+    evenimente[eventId].isFavorite = !evenimente[eventId].isFavorite;
+  }
 
-function favoriteCard(card){
-    card.classList.toggle("favorite");
-
+  let child = card.querySelector(".button-container");
+  console.log(child);
+  let favElement = child.querySelector(".star");
+  favElement.classList.toggle("favorite");
+  console.log(favElement);
 }
 
-function editCard(card){
-    let btnEdit = card.querySelector(".edit");
-    card.childNode.removeChild(btnEdit);
+function editCard(card) {
+  // console.log(card.querySelector("div").querySelector(".edit"));
+  let parent = card.querySelector("div");
+  let child = card.querySelector("div").querySelector(".edit");
+  let description = card.querySelector(".card-desc");
 
-    let description = card.querySelector(".card-desc");
-    let inputDesc = document.createElement("input");
+  parent.removeChild(child);
 
-    inputDesc.classList.add("editable");
-    inputDesc.setAttribute("type", "text");
+  let inputDesc = document.createElement("input");
 
-    inputDesc.value=description.textContent;
-    description.replaceWith(inputDesc);
+  card.appendChild(inputDesc);
 
-    let btnSave = card.querySelector(".save");
-    btnSave.classList.toggle("hide");
-    btnSave.classList.toggle("inline");
+  inputDesc.classList.add("editable");
+  inputDesc.setAttribute("type", "text");
 
+  // console.log(inputDesc.parentNode);
+
+  inputDesc.value = description.textContent;
+  description.replaceWith(inputDesc);
+
+  let btnSave = parent.querySelector(".save");
+  btnSave.classList.toggle("hide");
+  btnSave.classList.toggle("inline");
 }
 
-function saveCard(card){
-    let inputDesc = card.querySelector(".editable");
-    let description = document.createElement("p");
+function saveCard(card) {
+  let inputDesc = card.querySelector(".editable");
+  let description = document.createElement("p");
 
-    description.classList.add("card-desc");
-    description.textContent = (inputDesc.value);
-    inputDesc.replaceWith(description);
-
+  description.classList.add("card-desc");
+  description.textContent = inputDesc.value;
+  inputDesc.replaceWith(description);
 }
- 
-
 
 //functie ce gaseste pozitia unui obiect din arr dupa Id
 
-function findEvenimentById(arr,id){
-    for(let i=0; i<arr.length; i++){
-        if(arr[i].id === id){
-        return i;
-        }
+function findEvenimentById(arr, id) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].id === id) {
+      return i;
     }
+  }
 }
-
 
 //functie de stergere obiect din arr dupa id
 
-function deleteEvenimentById(arr, id){
-    return arr.filter(el=>el.id!=id);
-
+function deleteEvenimentById(arr, id) {
+  return arr.filter((el) => el.id != id);
 }
+
+function filterByType(arr,type) {
+  return  arr.filter((obj) => obj.type === type);
+}
+
+function filterByFav(arr){
+  return arr.filter((obj) => obj.isFavorite === true);
+}
+
+function handleFilterByType(arr) {
+  attachCards(arr);
+  if (arr.length < 1) {
+    noData.classList.remove("hide");
+  } else {
+    noData.classList.add("hide");
+  }
+}
+
+// push new card to the data.js database
+// when adding Notes, i want the system to stop returning me to allEvents
